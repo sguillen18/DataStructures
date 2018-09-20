f86d533176234331196f485334e8973282cba307
sguillen18 <36303985+sguillen18@users.noreply.github.com>
2018-09-20 16:31:49 -0400

    Add files via upload

---
+++ GuessingGame.java
@@ -0,0 +1,14 @@
+/*
+ * user input: must pick n, as to guess from 0 -> n. Also must pick amount of entries in the linkedbag
+ * after user inputs n, they must start guessing the numbers in the bag
+ * 
+ * generate list of numbers using the users parameters
+ * 
+ * take in a users guess, make it a linked bag.
+ * 
+ * run equals() to see if it matches. if so, then they won.
+ * if not, must tell them how many they got correct
+ */
+public class GuessingGame {
+
+}
