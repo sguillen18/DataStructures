e6fd74a2e2844525bd2b46e3f80894eb813a3ec8
trzsroo <33383490+trzsroo@users.noreply.github.com>
2018-09-20 16:35:23 -0400

    Add files via upload
    Adding the first input things

--- TestBag.java
+++ TestBag.java
@@ -1,9 +1,21 @@
  import instructor.ResArrayBag;
+ import java.util.Scanner;
  
 public class TestBag {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int numberOfNums;
+		int numbersRange;
+		
+		System.out.println("How many numbers do you want to guess?");
+		numberOfNums = sc.nextInt();
+		
+		System.out.println("Select a range from 1 to N.");
+		System.out.println("N:");
+		numbersRange = sc.nextInt();
+		
 
 	}
 
