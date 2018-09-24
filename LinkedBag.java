426710754693d1c37ee0b2317d35d0ab8e144b9b
trzsroo <33383490+trzsroo@users.noreply.github.com>
2018-09-24 09:24:18 -0400

    Add files via upload
    Getters and setters were being dumb in Node class, so I commented them out and just got them because we aren't making them public. I can explain anything that might be confusing

--- LinkedBag.java
+++ LinkedBag.java
@@ -21,7 +21,7 @@ public class LinkedBag <T> implements BagInterface <T> {
 			this(newData, null);
 		}
 		
-		public T getData() {
+		/*public T getData() {
 			return data;
 		}
 		
@@ -29,13 +29,13 @@ public class LinkedBag <T> implements BagInterface <T> {
 			data = newData;
 		}
 		
-		public Object getNext() {
+		public T getNext() {
 			return next;
 		}
 		
 		public void setNext(Node thing) {
 			next = thing;
-		}
+		}*/
 	}
 	
 	//returns number of Nodes
@@ -52,36 +52,64 @@ public class LinkedBag <T> implements BagInterface <T> {
 	public boolean add(T newEntry) {
 		Node newNode = new Node(newEntry);
 		newNode.next = firstNode;
+		firstNode = newNode;
 		numberOfElements ++;
 		return true;
 	}
 	
 	//removes one of the elements given in the bag
-	//returns true if wasa able to remove the item
+	//returns true if was able to remove the item
 	//otherwise false
 	public boolean remove(T anEntry) {
-		for(Node curr = firstNode; curr != null; curr = curr.getNext();)
+		for(Node curr = firstNode; curr != null; curr = curr.next) {
+			if(anEntry.equals(curr.data)) {
+				curr.data = firstNode.data;
+				firstNode = firstNode.next;
+				numberOfElements--;
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	//removes last object in bag
 	//returns true if can remove an item
 	public boolean remove() {
-		return false;
+		if(firstNode != null) {
+			firstNode = firstNode.next;
+			numberOfElements--;
+			return true;
+		}
+		else return false;
 	}
 	
 	//empty bag
 	public void clear() {
-		
+		for(Node curr = firstNode; curr != null; curr = curr.next) {
+			curr.data = null;
+			numberOfElements--;
+		}	
 	}
 	
 	//returns true if the bag contains given element
 	public boolean contains(T anEntry) {
+		for(Node curr = firstNode; curr != null; curr = curr.next) {
+			if(anEntry.equals(curr.data)) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	//returns how many times an item appears in a bag
 	public int getFrequencyOf (T anEntry) {
-		return 0;
+		int counter = 0;
+		for(Node curr = firstNode; curr != null; curr = curr.next) {
+			if(anEntry.equals(curr.data)) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 	
 	//creates bag into an array
@@ -92,7 +120,7 @@ public class LinkedBag <T> implements BagInterface <T> {
 		for (Node currentNode = firstNode; currentNode != null; currentNode = currentNode.next, count++) {
 			result[count] = currentNode.data;
 		}
-		return null;
+		return result;
 	}
 
 }
