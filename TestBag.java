5deef86fefeb55ebec489886b865dacbdbbea875
sguillen18 <36303985+sguillen18@users.noreply.github.com>
2018-09-24 17:56:45 -0400

    Add files via upload

--- TestBag.java
+++ TestBag.java
@@ -9,12 +9,34 @@ public class TestBag {
 		int numberOfNums;
 		int numbersRange;
 		
+		//create bag for numbers being guessed
+		LinkedBag <Integer> userGuess = new LinkedBag <> ();
+		
 		System.out.println("How many numbers do you want to guess?");
 		numberOfNums = sc.nextInt();
 		
 		System.out.println("Select a range from 1 to N.");
 		System.out.println("N:");
 		numbersRange = sc.nextInt();
+		
+		//while userGuess != correct bag
+		while() {
+			System.out.println("Enter " + numberOfNums + " integers in the range from 1 to " + numbersRange + ". Entries may be duplicate.");
+			
+			
+			//adding guesses to bag
+			for(int i = 0; i < numberOfNums; i++) {
+				int guess = sc.nextInt();
+				userGuess.add(guess);
+			}
+		}
+
+		
+	
+		
+		//write all the stuff the user sees when they play the game.
+		
+		//compare and return correct integers
 		
 
 	}
